60ce046e2313412f483de069318293168e5ada1a
Thera Manuel <theramanuel@gmail.com>
2024-06-28 16:55:13 -0700

    Added Functionality to Hire Method
    Finished Hire method functionality.
    Added an additional method to search for position.

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,10 +1,14 @@
 package com.aa.act.interview.org;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Iterator;
+import java.util.Set;
 
 public abstract class Organization {
 
     private Position root;
+    private int identifierNumber = 0;
     
     public Organization() {
         root = createOrganization();
@@ -21,7 +25,41 @@ public abstract class Organization {
      */
     public Optional<Position> hire(Name person, String title) {
         //your code here
+        //Create a new employee
+        identifierNumber += 1;
+        Employee newHire = new Employee(identifierNumber, person);
+
+        //Add employee with the position that has the given title, starting with root first
+        Position newPosition = positionCheck(root, title);
+        if(newPosition != null) {
+            newPosition.setEmployee(Optional.of(newHire));
+            return Optional.of(newPosition);
+        }
+
         return Optional.empty();
+    }
+
+    //Added a new method to find and check if position exists
+    public Position positionCheck (Position position, String title) {
+        Position correctPosition = null;
+
+        if(position.getTitle() == title)
+        {
+            return position;
+        }
+        else {
+            Iterator<Position> iterator = position.getDirectReports().iterator();
+            while(iterator.hasNext())
+            {
+                Position currentPosition = iterator.next();
+                correctPosition = positionCheck(currentPosition, title);
+                if(correctPosition != null){
+                    return correctPosition;
+                }
+
+            }
+        }
+        return null;
     }
 
     @Override
